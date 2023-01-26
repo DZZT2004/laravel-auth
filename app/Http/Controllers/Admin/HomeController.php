afde9a1b8db3b26d29846f7893e1a987d782beeb
DZZT2004 <taodaiuwu@gmail.com>
2023-01-27 00:42:11 +0100

    first commit

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        return view('home', compact('user'));
+    }
+}
